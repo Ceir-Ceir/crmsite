ba8813affcdcdd0c6b01a0e8e624f959d02b4e62
ceir-ceir <ericgreen314@gmail.com>
2025-05-13 21:44:24 -0400

    deploy

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // enables static export (Next.js 13+)
-  trailingSlash: true,
-  basePath: '', // REPO_NAME = your GitHub repo name
+  output: 'export'
 };
 
 module.exports = nextConfig;
