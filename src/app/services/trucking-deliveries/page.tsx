b1b95c623345a7caa177435cd2054a3cd4d90422
ceir-ceir <ericgreen314@gmail.com>
2025-05-19 21:27:17 -0400

    trucking update

--- src/app/services/trucking-deliveries/page.tsx
+++ src/app/services/trucking-deliveries/page.tsx
@@ -114,7 +114,7 @@ export default function TruckingDeliveriesPage() {
           playsInline
           className="absolute inset-0 w-full h-full object-cover"
         >
-          <source src="/assets/Truckinghero2.mp4" type="video/mp4" />
+          <source src="/assets/Truckinghero2.MP4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
         <div className="absolute inset-0 bg-black bg-opacity-50" />
