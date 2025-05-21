e143bd4b95a64dfe5abe9a3bcd302fd5ab9d847b
ceir-ceir <ericgreen314@gmail.com>
2025-05-21 19:40:40 -0400

    gitpages

--- next.config.js
+++ next.config.js
@@ -4,8 +4,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/crmsite' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/crmsite/' : '',
 }
 
 module.exports = nextConfig
