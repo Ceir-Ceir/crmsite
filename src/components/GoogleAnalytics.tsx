48151e94d4f29b9707e1f0e19404a45033d8dc2e
ceir-ceir <ericgreen314@gmail.com>
2025-06-03 14:48:52 -0400

    fixed google tag

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -3,10 +3,13 @@
 import Script from "next/script";
 
 export default function GoogleAnalytics() {
+  const gtagId = process.env.NEXT_PUBLIC_GTAG_ID;
+  if (!gtagId) return null;
+
   return (
     <>
       <Script
-        src="https://www.googletagmanager.com/gtag/js?id=AW-17073008016"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtagId}`}
         strategy="afterInteractive"
       />
       <Script id="google-analytics" strategy="afterInteractive">
@@ -14,9 +17,9 @@ export default function GoogleAnalytics() {
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', 'AW-17073008016');
+          gtag('config', '${gtagId}');
         `}
       </Script>
     </>
   );
-} 
+}
